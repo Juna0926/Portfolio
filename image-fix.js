07233a32811a65d43434d15e3cdaac53806248d2
Juna0926 <wonjunha12@ajou.ac.kr>
2026-09-09 15:30:14 +0900

    Load KNHANES figure from a single verified PNG source

--- image-fix.js
+++ image-fix.js
@@ -38,18 +38,12 @@
     const image=figure?.querySelector('img');
     if(!figure||!image) return;
 
-    const src=await loadPngData([
-      '../assets/pngdata/k0.b64','../assets/pngdata/k1.b64','../assets/pngdata/k2.b64',
-      '../assets/pngdata/k3.b64','../assets/pngdata/k4.b64','../assets/pngdata/k5.b64',
-      '../assets/pngdata/k6.b64','../assets/pngdata/k7.b64','../assets/pngdata/k8.b64',
-      '../assets/pngdata/k9a.b64','../assets/pngdata/k9b.b64','../assets/pngdata/k9c.b64',
-      '../assets/pngdata/k9d.b64','../assets/pngdata/k9e.b64',
-      '../assets/pngdata/k9f1.b64','../assets/pngdata/k9f2.b64',
-      '../assets/pngdata/k9f3.b64','../assets/pngdata/k9f4.b64'
-    ]);
+    const src=await loadPngData(['../assets/media/project-knhanes-detail.png.b64']);
     figure.dataset.src=src;
     image.src=src;
     image.removeAttribute('onerror');
+    image.style.objectFit='contain';
+    image.style.background='#fff';
   };
 
   Promise.all([setHospitalCover(),setKnhanesMainFigure()]).catch(error=>console.error('[portfolio image fix]',error));
